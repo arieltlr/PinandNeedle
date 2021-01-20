c515dcd8d1314d4065554a23959d03089db3cb0e
ariel taylor <ariel.taylorr@gmail.com>
2021-01-19 16:39:51 -0800

    remove console logs - everything works now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
-console.log("application")
 
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    console.log("index")
     const root = document.getElementById("root");
     
     ReactDOM.render(<h1>Welcome to PinandNeedle!</h1>, root)
