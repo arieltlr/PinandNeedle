9976a7d970a782af6e2e4e8c6b359dffd7875a2f
ariel taylor <ariel.taylorr@gmail.com>
2021-01-20 07:50:33 -0800

    Initial branch commit

---
+++ frontend/reducers/user_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const userReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser.username })
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
