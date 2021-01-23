8779f4f5449f9268a793613d1621610b465f2de9
ariel taylor <ariel.taylorr@gmail.com>
2021-01-23 15:15:37 -0800

    Style navigation bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -6,13 +6,17 @@ class NavBar extends React.Component {
         return (
             <div>
                 <nav className="nav-bar">
-                    <img className="nav-ele" id="header-logo" src={window.logo} alt="needle_icon" />
+                    <div className="nav-ele" id="header-logo-circle"  alt="needle_icon" ></div>
                     <button className="nav-ele"id="nav-home">Home</button>
                     <button className="nav-ele"id="nav-following">Following</button>
-                    <input className="nav-ele"id="search-bar" type="text" placeholder="Search"/>
-                    <img className="nav-ele"id="notification-icon" src={window.notification} alt="notification bell"/>
-                    <button className="nav-ele"id="profile-button">{this.props.user.username[0]}</button>
-                    <img className="nav-ele" id="down-icon" src={window.down_icon} alt="down-icon"/>
+                    {/* <div className="nav-ele"> */}
+                    <input className="nav-ele" id="search-bar" type="text" placeholder="Search" />
+                        {/* <i className="nav-ele" id="search-icon" src={window.search_icon} alt="magnifying-glass"></i> */}
+                    {/* </div> */}
+                    {/* <img className="nav-ele"id="notification-icon" src={window.notification} alt="notification bell"/> */}
+
+                    <p className="nav-ele"id="profile-button">{this.props.user.username[0]}</p>
+                    <div className="nav-ele" id="down-icon-circle" alt="down-icon"></div>
 
                 </nav>
                 <button id="holder" className="red-button" onClick={this.props.logout}>Logout</button>
