78715ef9ca33c887d4e4aa29fb73b84262143cf3
ariel taylor <ariel.taylorr@gmail.com>
2021-01-28 10:22:38 -0800

    Add hover effect to board show icons

--- frontend/components/boards/board_show.jsx
+++ frontend/components/boards/board_show.jsx
@@ -73,7 +73,19 @@ class BoardShow extends React.Component {
                                     <p id="small-profile-page-letter">{profileLetter}</p>
                                 </div>
                                 {/* </Link> */}
-                                <BoardOptions currentUsersBoard={currentUsersBoard} emailName={emailName} />
+                                {/* <BoardOptions currentUsersBoard={currentUsersBoard} emailName={emailName} /> */}
+                                <div className="icon-options-container">
+                                    <div className="ideas-container">
+                                        <Link to="/feed">
+                                            <div className="ideas-icon"></div>
+                                            <p className="icon-text">More ideas</p>
+                                        </Link>
+                                    </div>
+                                    <div className="notes-container">
+                                        <div className="notes-icon" onClick={() => this.props.openModal("edit-board")}></div>
+                                        <p className="icon-text">Notes</p>
+                                    </div>
+                                </div>
 
                             </div>
                             {/* <BoardOptions currentUsersBoard={currentUsersBoard} emailName={emailName} /> */}
